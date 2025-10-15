ee8f6fc72093db94fa71ac10daf4bb2465aaa0af
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2025-10-15 22:25:44 +0700

    feat: Making Publisher Factory

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Publisher extends Model
 {
+
+    use HasFactory;
+
     protected $guarded = ['id'];
 
     public function books()
