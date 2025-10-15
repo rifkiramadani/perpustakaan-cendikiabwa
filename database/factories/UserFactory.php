f6906518ef006bfc04af0a5fdbfe4ad96106f81a
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2025-10-15 22:07:50 +0700

    feat: Modifying User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            //tambah attribute factory baru
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
