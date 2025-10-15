0d85bda9538360f4c2a6ab52f8836193b64678b7
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2025-10-15 22:18:48 +0700

    feat: Making Category Factory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
+    use HasFactory; //agar bisa menggunakan factory
+
     protected $guarded = ['id'];
 
     public function books()
---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $name = $this->fake()->unique()->randomElement([
+                'Fiksi',
+                'Non-Fiksi',
+                'Ilmu Pengetahuan',
+                'Sejarah',
+                'Biografi',
+                'Anak-Anak',
+                'Teknologi',
+                'Fantasi',
+                'Pengembangan Diri',
+                'Misteri'
+            ]),
+            'slug' => str()->lower(str()->slug($name) . str()->random(4)),
+        ];
+    }
+}
